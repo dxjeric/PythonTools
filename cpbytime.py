f0dc66505b3075ef894688d00f6a6f6db6384a20
eric <dangxiaojie@sygame.com>
2020-11-04 12:01:54 +0800

    优化

--- cpbytime.py
+++ cpbytime.py
@@ -1,10 +1,21 @@
 import sys,os,io,time, shutil, datetime
 
-targetDir       = "f:/12121"
-transferPath	= "C:/Users/dangxiaojie/Documents/Tencent Files/3289685120/Image"
+targetDir   = "f:/12121"
+imagePath	= "C:/Users/dangxiaojie/Documents/Tencent Files/3289685120/Image"
+videoPath   = "C:/Users/dangxiaojie/Documents/Tencent Files/3289685120/Video"
+
+def processDir(pdir, beginTime, endTime):
+    for r, d, files in os.walk(pdir):
+        for f in files:
+            filepath = r + "/" + f
+            mtime = os.path.getmtime(filepath)
+            if mtime >= beginTime and mtime <= endTime :
+                filename = os.path.basename(filepath)
+                print(filepath)
+                shutil.copy(filepath, targetDir + "/" + filename)
 
 def main():
-    dt = datetime.datetime.strptime("2020-11-04 10:09:00", "%Y-%m-%d %H:%M:%S")
+    dt = datetime.datetime.strptime("2020-10-22 20:06:00", "%Y-%m-%d %H:%M:%S")
     # now = time.time()
     beginTime = dt.timestamp() - 60
     endTime   = dt.timestamp() + 60
@@ -16,13 +27,8 @@ def main():
                 log("create dir erorr! %s", os.strerror(e.errno))
                 return False
 
-    for r, d, files in os.walk(transferPath):
-        for f in files:
-            filepath = r + "/" + f
-            mtime = os.path.getmtime(filepath)
-            if mtime >= beginTime and mtime <= endTime :
-                filename = os.path.basename(filepath)
-                print(filepath)
-                shutil.copy(filepath, targetDir + "/" + filename)
+    processDir(imagePath, beginTime, endTime)
+    processDir(videoPath, beginTime, endTime)
+
 
 main()
