4c1ea3c85319147f0075f60132b97a9d40f7b78f
eric <frederick.dang@gmail.com>
2023-11-28 10:44:06 +0800

    优化

--- cpbytime.py
+++ cpbytime.py
@@ -3,9 +3,8 @@ import sys,os,io,time, shutil, datetime
 exts        = [".gif", ".jpg", ".png", ".mp4", ".bmp"]
 targetRoot  = "f:/Pic"
 imagePath	= "C:/Users/dangxiaojie/Documents/Tencent Files/3289685120"
-# timestr     = "2021-01-28 17:08:00"
-timestr     = ""
-interval    = 1
+#timestr     = "2023-11-28 09:55:00"
+interval    = 5*60
 
 
 def processDir(pdir, beginTime, endTime, fileBase, beginIndex, targetDir):
@@ -13,6 +12,8 @@ def processDir(pdir, beginTime, endTime, fileBase, beginIndex, targetDir):
         for f in files:
             filepath = r + "/" + f
             mtime = os.path.getmtime(filepath)
+#            print("filepath: ", filepath, "mtime: ", mtime)
+#            print("mtime >= beginTime: ", (mtime >= beginTime), "mtime <= endTime: ", (mtime <= endTime))
             if mtime >= beginTime and mtime <= endTime :
                 filename = os.path.basename(filepath)
                 ext = os.path.splitext(filename)[-1].lower()
@@ -25,13 +26,14 @@ def processDir(pdir, beginTime, endTime, fileBase, beginIndex, targetDir):
 def main():
     dt = datetime.datetime.now()
 
-    if timestr != "":
+    if 'timestr' in globals().keys():        
         dt = datetime.datetime.strptime(timestr, "%Y-%m-%d %H:%M:%S")
         
     targetDir = targetRoot + "/" + dt.strftime("%Y%m%d")
     fileBase = dt.strftime("%Y%m%d_%H%M%S")
     beginTime = dt.timestamp() - interval
     endTime   = dt.timestamp() + interval
+    print("beginTime: ", beginTime, "endTime: ", endTime)
     if not os.path.isdir(targetDir):
         try:
             os.makedirs(targetDir)
