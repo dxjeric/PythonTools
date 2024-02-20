34227140c79f605af8412265e141e5b06bedd39b
eric <frederick.dang@gmail.com>
2024-02-20 10:33:07 +0800

    增加日志文件

--- zlibbook.py
+++ zlibbook.py
@@ -19,8 +19,9 @@ def main():
         for f in files:
             all_books.append(r + "/" + f)
     
-    print("all_books: ", all_books, "len(all_books)", len(all_books))
+    # print("all_books: ", all_books, "len(all_books)", len(all_books))
     zipBase = os.path.basename(dirPath)
+    logfile = open(zipBase + ".txt", "w")    
     partIndex = 0
     bookIndex = 0
     totalFileCount = 0
@@ -37,10 +38,12 @@ def main():
             stats = os.stat(fp)
             totalSize += stats.st_size
             zipFile.write(fp, fp, zipfile.ZIP_DEFLATED)
+        logfile.write("index: {}, zip file: {}, total: {}\n".format(partIndex, curZipFileCount, totalFileCount))
         print("curZipFileCount:", curZipFileCount, "totalFileCount:", totalFileCount, "totalSize:", totalSize, "maxZipFileSize:", maxZipFileSize)
         zipFile.close()
     
-    print("totalFileCount: ", totalFileCount) 
+    print("finsh totalFileCount: ", totalFileCount)
+    logfile.close()
 
 if __name__ == "__main__":
-	main()
+	main()    
