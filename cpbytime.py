759f7fddbc4347f4789bb2098cf6a33ce532931d
eric <frederick.dang@gmail.com>
2024-04-01 21:43:08 +0800

    format

--- cpbytime.py
+++ cpbytime.py
@@ -1,42 +1,46 @@
-import sys,os,io,time, shutil, datetime, string
+import sys, os, io, time, shutil, datetime, string
 
-exts        = [".gif", ".jpg", ".png", ".mp4", ".bmp"]
-targetRoot  = "f:/Pic"
-imagePath	= "C:/Users/dangxiaojie/Documents/Tencent Files/3289685120/nt_qq/nt_data"
-timestr     = "2024-04-01 14:12:00"
-interval    = 15*60
+exts = [".gif", ".jpg", ".png", ".mp4", ".bmp"]
+targetRoot = "f:/Pic"
+imagePath = "C:/Users/dangxiaojie/Documents/Tencent Files/3289685120/nt_qq/nt_data"
+timestr = "2024-04-01 14:12:00"
+interval = 15 * 60
 findDirs = ["Pic", "Video"]
 
-def processDir(pdir, beginTime, endTime, fileBase, beginIndex, targetDir):    
+
+def processDir(pdir, beginTime, endTime, fileBase, beginIndex, targetDir):
     for r, d, files in os.walk(pdir):
         # print("r", os.path.basename(r))
         if os.path.basename(r) != "Ori":
             continue
-        for f in files:            
+        for f in files:
             filepath = r + "/" + f
             mtime = os.path.getmtime(filepath)
             # print("filepath: ", filepath, "mtime: ", mtime)
             # print("mtime >= beginTime: ", (mtime >= beginTime), "mtime <= endTime: ", (mtime <= endTime))
-            if mtime >= beginTime and mtime <= endTime :
+            if mtime >= beginTime and mtime <= endTime:
                 filename = os.path.basename(filepath)
                 ext = os.path.splitext(filename)[-1].lower()
                 if ext in exts:
                     print(filepath)
                     beginIndex = beginIndex + 1
-                    shutil.copy(filepath, targetDir + "/" + fileBase + "{:0>3d}".format(beginIndex) + ext)
+                    shutil.copy(
+                        filepath, targetDir + "/" + fileBase +
+                        "{:0>3d}".format(beginIndex) + ext)
 
     return beginIndex
+
 
 def main():
     dt = datetime.datetime.now()
 
     if 'timestr' in globals().keys():
         dt = datetime.datetime.strptime(timestr, "%Y-%m-%d %H:%M:%S")
-        
+
     targetDir = targetRoot + "/" + dt.strftime("%Y%m%d")
     fileBase = dt.strftime("%Y%m%d%H%M%S")
     beginTime = dt.timestamp() - interval
-    endTime   = dt.timestamp() + interval
+    endTime = dt.timestamp() + interval
     print("beginTime: ", beginTime, "endTime: ", endTime)
     if not os.path.isdir(targetDir):
         try:
@@ -51,9 +55,11 @@ def main():
     for _dir in findDirs:
         path_dir = imagePath + "/" + _dir
         print("path_dir", path_dir)
-        beginIndex = processDir(path_dir, beginTime, endTime, fileBase, beginIndex, targetDir)
+        beginIndex = processDir(path_dir, beginTime, endTime, fileBase,
+                                beginIndex, targetDir)
 
-    print("total: ", beginIndex)    
+    print("total: ", beginIndex)
     os.system("explorer.exe %s" % targetDir.replace("/", "\\"))
+
 
 main()
--- zlibbook.py
+++ zlibbook.py
@@ -3,26 +3,28 @@ import sys, os, zipfile, time
 MAX_ZIP_FILE_SIZE = 1024 * 1024
 RM_AFTER_ZIP = False
 
+
 def main():
     print("sys.argv: ", len(sys.argv), str(sys.argv[0]))
-    dirPath = str(sys.argv[1])    
+    dirPath = str(sys.argv[1])
     all_books = []
 
     maxZipFileSize = -1
     if 'MAX_ZIP_FILE_SIZE' in globals().keys():
         maxZipFileSize = MAX_ZIP_FILE_SIZE
-    
+
     if dirPath[-1] == '/':
         dirPath = dirPath[0:-1]
-    
+
     for r, _, files in os.walk(dirPath):
         for f in files:
             all_books.append(r + "/" + f)
-    
+
     # print("all_books: ", all_books, "len(all_books)", len(all_books))
-    print("dirPath", dirPath, "maxZipFileSize", maxZipFileSize, "all_books count: ", len(all_books))
+    print("dirPath", dirPath, "maxZipFileSize", maxZipFileSize,
+          "all_books count: ", len(all_books))
     zipBase = os.path.basename(dirPath)
-    logfile = open(zipBase + ".txt", "w")    
+    logfile = open(zipBase + ".txt", "w")
     partIndex = 0
     bookIndex = 0
     totalFileCount = 0
@@ -32,12 +34,13 @@ def main():
         partIndex += 1
         totalSize = 0
         zipfillename = r'{}.part{:0>2d}.zip'.format(zipBase, partIndex)
-        if maxZipFileSize == -1 : 
+        if maxZipFileSize == -1:
             zipfillename = r'{}.zip'.format(zipBase)
         allZipFileNames.append(zipfillename)
         zipFile = zipfile.ZipFile(zipfillename, 'w')
         curZipFileCount = 0
-        while bookIndex < len(all_books) and (maxZipFileSize == -1 or totalSize < maxZipFileSize):
+        while bookIndex < len(all_books) and (maxZipFileSize == -1
+                                              or totalSize < maxZipFileSize):
             totalFileCount += 1
             curZipFileCount += 1
             fp = all_books[bookIndex]
@@ -46,17 +49,20 @@ def main():
             totalSize += stats.st_size
             zipFile.write(fp, fp, zipfile.ZIP_DEFLATED)
             if RM_AFTER_ZIP:
-                os.remove(fp) # 删除文件
+                os.remove(fp)  # 删除文件
         zipFile.close()
         cur_time = time.time() * 1000
-        log_str = "zipfillename: {},  curZipFileCount: {}, totalFileCount: {}, spendTime: {}".format(zipfillename, curZipFileCount, totalFileCount, int(cur_time-begin_time))
+        log_str = "zipfillename: {},  curZipFileCount: {}, totalFileCount: {}, spendTime: {}".format(
+            zipfillename, curZipFileCount, totalFileCount,
+            int(cur_time - begin_time))
         logfile.write(log_str)
         print(log_str)
         begin_time = cur_time
     logfile.write("\n\n")
     logfile.close()
     os.system("md5sum {}.part0*.zip >> {}.txt".format(zipBase, zipBase))
-    print("finsh totalFileCount: ", totalFileCount)    
+    print("finsh totalFileCount: ", totalFileCount)
+
 
 if __name__ == "__main__":
-	main()    
+    main()
