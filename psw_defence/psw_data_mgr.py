5a24e58d4e8ea8202e7fc969dc83a52b889a0421
eric <frederick.dang@gmail.com>
2024-05-13 20:58:12 +0800

    更新

--- psw_defence/psw_data_mgr.py
+++ psw_defence/psw_data_mgr.py
@@ -1,6 +1,7 @@
 import random, time
 
 
+# 密码随机
 class PassWordRandom():
 
     def __init__(self):
@@ -35,6 +36,16 @@ class PassWordRandom():
 
         _all_chars.clear()
         return new_ps_str
+
+
+# 密文数据
+class EnCryptData():
+
+    def __init__(self):
+        self.md5_check = ""  # 明文秘钥转为MD5后存储使用
+        self.rsa_public_key = ""  # 这里为密文
+        self.rsa_private_key = ""  # 这里为密文
+        self.encrypte_json = None  # json数据
 
 
 # 测试
--- psw_defence/qt_windown.py
+++ psw_defence/qt_windown.py
@@ -1,7 +1,8 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 # from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 from psw_defence_GUI import Ui_mainWindow
+from psw_data_mgr import PassWordRandom, EnCryptData
 
 
 class QT_MainWindow(QtWidgets.QMainWindow, Ui_mainWindow):
@@ -10,6 +11,16 @@ class QT_MainWindow(QtWidgets.QMainWindow, Ui_mainWindow):
         super(QT_MainWindow, self).__init__(parent)
         self.setupUi(self)
         self.setFixedSize(self.width(), self.height())
+        self.randomPW = PassWordRandom()
+        self.encryptData = EnCryptData()
+        self.PushButton_SelPWFile.clicked.connect(self.selectEcryptFile)
+
+    def selectEcryptFile(self):
+        msg_box = QtWidgets.QMessageBox(self)
+        msg_box.setText("就是这个!")
+        msg_box.setWindowTitle("密码盾")
+        msg_box.setStyleSheet("QLabel{min-width: 150px;min-height: 50px;}")
+        msg_box.show()
 
 
 app = QtWidgets.QApplication(sys.argv)
